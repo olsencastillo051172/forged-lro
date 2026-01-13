aec59c4767cbf750c0bcf6e2a6b764fd04af243e
olsencastillo051172 <olsencastillo92@gmail.com>
2026-01-13 03:15:47 -0400

    Create auth.go

---
+++ auth/auth.go
@@ -0,0 +1,11 @@
+package auth
+
+// Authenticate checks user credentials (stub)
+func Authenticate(user string, password string) bool {
+    return false
+}
+
+// Authorize checks if a role can perform an action (stub)
+func Authorize(role string, action string) bool {
+    return false
+}
